384fdc33649b79becbf9c16971da3c99d77cfebe
eliferretti <eliferretti14@gmail.com>
2022-02-21 16:10:15 -0300

    tentativa join

--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Andor.Models;
 
 namespace Andor.Models
 {
@@ -27,8 +28,8 @@ namespace Andor.Models
             // conexao somee.com
             // optionsBuilder.UseSqlServer(connectionString: @"workstation id = DBANDOR.mssql.somee.com; packet size = 4096; user id = Ferretti_SQLLogin_1; pwd = khoi5d5m8w; data source = DBANDOR.mssql.somee.com; persist security info = False; initial catalog = DBANDOR");
 
-
-
         }
+
+        public DbSet<Andor.Models.Profissional> Profissional { get; set; }
     }        
 }
---
+++ Models/Profissional.cs
@@ -0,0 +1,8 @@
+﻿namespace Andor.Models
+{
+    public class Profissional : Formacao
+    {
+        public string PessoaNome { get; set; }
+        public string PessoaUF { get; set; }
+    }
+}
