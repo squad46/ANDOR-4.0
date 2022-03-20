29bfe269808e110936cf2d975c34478e096cd4ba
Christian-M-Silva <hoodprogrammer@gmail.com>
2022-03-20 15:41:22 -0300

    Criei e estilizei a pág Revalidação

---
+++ Controllers/RevalidacaoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Andor.Controllers
+{
+    public class RevalidacaoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
